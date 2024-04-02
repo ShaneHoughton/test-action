ad147d4392d27833415d02042803e3f2b0934f0d
Shane Houghton <shanegh13@gmail.com>
2024-04-02 11:18:52 -0400

    feat/1 - uncomment test

--- tests/add.test.js
+++ tests/add.test.js
@@ -9,7 +9,7 @@ describe('Tests inside of test-action', () => {
     expect(add(2,3)).toBe(4);
   });
 
-  // test('2 + 2 is 5', async () => {
-  //   expect(add(2,6)).toBe(5);
-  // });
+  test('2 + 2 is 5', async () => {
+    expect(add(2,6)).toBe(5);
+  });
 });
