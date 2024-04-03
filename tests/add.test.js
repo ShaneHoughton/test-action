56082e46914a564dd46f57ec18f2cbacfb5d2053
Shane Houghton <shanegh13@gmail.com>
2024-04-02 21:10:29 -0400

    feat/1 - fix tests

--- tests/add.test.js
+++ tests/add.test.js
@@ -6,10 +6,10 @@ describe('Tests inside of test-action', () => {
   });
 
   test('2 + 2 is 4', async () => {
-    expect(add(2,3)).toBe(4);
+    expect(add(2,2)).toBe(4);
   });
 
   test('2 + 2 is 5', async () => {
-    expect(add(2,6)).toBe(5);
+    expect(add(2,2)).toBe(4);
   });
 });
