ad5631688b3cdbcc1726e7874be03b6edee886c2
Shane Houghton <shanegh13@gmail.com>
2024-04-03 16:27:49 -0400

    feat/1 - make failing tests

--- tests/add.test.js
+++ tests/add.test.js
@@ -1,15 +1,15 @@
-const { add } = require('../src/mocks/add');
+const { add } = require("../src/mocks/add");
 
-describe('Tests inside of test-action', () => {
-  test('2 + 2 is 4', async () => {
-    expect(add(2,2)).toBe(4);
+describe("Tests inside of test-action", () => {
+  test("2 + 2 is 4", async () => {
+    expect(add(2, 2)).toBe(4);
   });
 
-  test('2 + 2 is 4', async () => {
-    expect(add(2,2)).toBe(4);
+  test("2 + 2 is 4", async () => {
+    expect(add("2", 2)).toBe(4);
   });
 
-  test('2 + 2 is 5', async () => {
-    expect(add(2,2)).toBe(4);
+  test("2 + 2 is 4", async () => {
+    expect(add(2, "2")).toBe(4);
   });
 });
